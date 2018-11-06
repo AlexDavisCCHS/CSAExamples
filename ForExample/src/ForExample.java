31fe09e06ea91bbbeaf6a9a1e680d057418cf3f7
Alex Davis <alexdavis@ccusd.org>
2018-11-05 17:27:30 -0800

    saving ForExample

---
+++ ForExample/src/ForExample.java
@@ -0,0 +1,11 @@
+
+public class ForExample {
+	public static void main(String[] args) {
+		for (int index = 0; index < 3; index++) {
+			System.out.println("HAHA Made you look!");
+			System.out.println(index);
+		}//end for
+		System.out.println("Stop looking");
+	}//end main
+
+}//end class
