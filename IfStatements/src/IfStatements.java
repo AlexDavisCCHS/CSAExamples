b81e1e80418023dbef9248b52f9bd42bb819ef86
Alex Davis <alexdavis@ccusd.org>
2018-11-06 09:47:29 -0800

    Save IF Example

---
+++ IfStatements/src/IfStatements.java
@@ -0,0 +1,18 @@
+
+public class IfStatements {
+	public static void main(String[] args) {
+		int number = 20;
+		
+		if (number > 20) {
+			System.out.println("The number is bigger than 20");
+		}//end if
+		else if (number > 10) {
+			System.out.println("The number is between 10 and 20");
+		}//end else if
+		else {
+			System.out.println("the number is smaller than 10");
+		}//end else
+		System.out.println("end of program");
+	}//end main
+
+}//end class
